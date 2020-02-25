9c0736e82aec847532f0b6283a9096bae06d63a6
Hadi <abdulhadi2422@outlook.com>
2020-02-25 14:14:26 +0000

    added error handling for when the db goes to sleep and the connection is lost.

--- dbConnection.js
+++ dbConnection.js
@@ -2,13 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-
-const connection = mysql.createConnection({
+const dbConfig = {
     host: 'eu-cdbr-west-02.cleardb.net',
     user: 'b5af0859c0faaa',
     password: '0127196f',
     database: 'heroku_f930aca5c566b3d'
-});
+}
+
+const connection = ''
+/*Code taken from https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection*/
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig)
+    
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err)
+            setTimeout(handleDisconnect, 2000) // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err)
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect()                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err                               // server variable configures this)
+        }
+    })
+}
+
 
 
 
@@ -24,9 +46,9 @@ app.use(function (req, res, next) {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-connection.connect();
 
 app.get('/products', function (req, res) {
+    handleDisconnect()
     connection.query('SELECT * FROM products', function (error, results, fields) {
         if (error) throw error;
         res.send(results)
@@ -34,6 +56,7 @@ app.get('/products', function (req, res) {
 })
 
 app.post('/active_orders', function (req, res) {
+    handleDisconnect()
     const active_order = { order: JSON.stringify(req.body) }
     console.log('sending order...')
     connection.query('INSERT INTO active_orders set ?', active_order, function (error, results, fields) {
@@ -45,7 +68,7 @@ app.post('/active_orders', function (req, res) {
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
-});
+})
