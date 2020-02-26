2457af6000bf24e74aa5bff966ccbc3c432f790c
Hadi <abdulhadi2422@outlook.com>
2020-02-26 15:54:12 +0000

    changing error messages

--- dbConnection.js
+++ dbConnection.js
@@ -22,7 +22,7 @@ function handleDisconnect() {
     });                                     // process asynchronous requests in the meantime.
     // If you're also serving http, display a 503 error.
     connection.on('error', function (err) {
-        console.log('reconnecting to database')
+        console.log('Connection was lost to database, reconnecting...')
         if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleDisconnect()                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
@@ -87,4 +87,3 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
 
-handleDisconnect()
